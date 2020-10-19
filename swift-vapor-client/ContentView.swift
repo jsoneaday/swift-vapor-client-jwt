f15b4232f65f711b96fbb8ca84f9d50a06363297
David Choi <admin@dzhaven.com>
2020-10-19 13:02:36 -0400

    Initial Commit

---
+++ swift-vapor-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-vapor-client
+//
+//  Created by David Choi on 10/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-vapor-client/swift_vapor_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_vapor_clientApp.swift
+//  swift-vapor-client
+//
+//  Created by David Choi on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_vapor_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
